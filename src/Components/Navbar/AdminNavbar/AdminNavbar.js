e85e2f6755cb41455d713b0e8a44e9440fcfb4a3
aashrav <aashrav.shetty01@gmail.com>
2020-08-22 01:41:54 -0700

    Added profile dropdown to navbar

--- src/Components/Navbar/AdminNavbar/AdminNavbar.js
+++ src/Components/Navbar/AdminNavbar/AdminNavbar.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './AdminNavbar.css'
-
+import ProfileDropdown from '../../ProfileDropdown/ProfileDropdown';
 const AdminNavbar = (props) =>{
+  const dropdownLinks = [
+    {
+      label:'Upload',
+      to: 'upload',
+      click: null
+    },
+    {
+      label: 'Log Out',
+      to: '/',
+      click: props.handleLogout
+    }
+  ]
   return(
       <div className = "admin-navbar">
         <img className = "adminnb-logo" alt = "" src = {process.env.PUBLIC_URL + '/images/logo.png'}></img>
@@ -16,13 +28,13 @@ const AdminNavbar = (props) =>{
           <Link className = "admin-link" to = "/">
             <h3>GALLERY</h3>
           </Link>
-        </button>
-        <button>
-          <Link className="admin-link" to= "contact">
-            <h3>CONTACT</h3>
-          </Link>
-        </button>
+          </button>
           <button>
+            <Link className="admin-link" to= "contact">
+              <h3>CONTACT</h3>
+            </Link>
+          </button>
+          {/* <button>
             <Link className = "admin-link" to= "upload">
               <h3>UPLOAD</h3>
             </Link>
@@ -36,7 +48,8 @@ const AdminNavbar = (props) =>{
             <Link className = "admin-link" to = '/' onClick = {props.handleLogout}>
               <h3>LOG OUT</h3>
             </Link>
-          </button>
+          </button> */}
+          <ProfileDropdown links = {dropdownLinks} handleLogout = {props.handleLogout} user = {props.user}></ProfileDropdown>
         </div>
       </div>
   );
--- src/Components/Navbar/StudentNavbar/StudentNavbar.js
+++ src/Components/Navbar/StudentNavbar/StudentNavbar.js
@@ -1,37 +1,51 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import ProfileDropdown from '../../ProfileDropdown/ProfileDropdown';
 import './StudentNavbar.css'
 
 const StudentNavbar = (props) =>{
+  const dropdownLinks = [
+    {
+      label:'Dashboard',
+      to: 'dashboard',
+      click: null
+    },
+    {
+      label: 'Log Out',
+      to: '/',
+      click: props.handleLogout
+    }
+  ]
   return(
       <div className = "student-navbar">
         <img className = "studentnb-logo" alt = "" src = {process.env.PUBLIC_URL + '/images/logoWhite.png'}></img>
         <div className = "studentnb-links">
-        <button>
-          <Link className = "student-link" to = "/">
-            <h3>HOME</h3>
-          </Link>
-        </button>
-        <button>
-          <Link className = "student-link" to = "/">
-            <h3>GALLERY</h3>
-          </Link>
-        </button>
-        <button>
-          <Link className="student-link" to= "contact">
-            <h3>CONTACT</h3>
-          </Link>
-        </button>
-        <button>
-          <Link className="student-link" to = 'dashboard'>
-            <h3>DASHBOARD</h3>
-          </Link>
-        </button>
-        <button>
-          <Link className="student-link" to = '/' onClick = {props.handleLogout}>
-            <h3>LOG OUT</h3>
-          </Link>
-        </button>
+          <button>
+            <Link className = "student-link" to = "/">
+              <h3>HOME</h3>
+            </Link>
+          </button>
+          <button>
+            <Link className = "student-link" to = "/">
+              <h3>GALLERY</h3>
+            </Link>
+          </button>
+          <button>
+            <Link className="student-link" to= "contact">
+              <h3>CONTACT</h3>
+            </Link>
+          </button>
+          {/* <button>
+            <Link className="student-link" to = 'dashboard'>
+              <h3>DASHBOARD</h3>
+            </Link>
+          </button>
+          <button>
+            <Link className="student-link" to = '/' onClick = {props.handleLogout}>
+              <h3>LOG OUT</h3>
+            </Link>
+          </button> */}
+          <ProfileDropdown links = {dropdownLinks} handleLogout = {props.handleLogout} user = {props.user}></ProfileDropdown>
         </div>
       </div>
   );
