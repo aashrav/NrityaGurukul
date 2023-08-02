af33b460251441271967cf9a1bcecd2e957e8261
unknown <aashrav.shetty01@gmail.com>
2023-08-02 01:33:29 -0700

    Updates

--- precache-manifest.97decfbaf7b8ae0271466d307a045050.js
+++ precache-manifest.97decfbaf7b8ae0271466d307a045050.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "eb80a4cfeb429e2c70e9c8306f207a56",
+    "revision": "841cd469152886d72c6ef1ff6751093d",
     "url": "/NrityaGurukul/index.html"
   },
   {
-    "revision": "411c49aa8ebd262458c4",
+    "revision": "92aaf8dffb21cebb76fc",
     "url": "/NrityaGurukul/static/css/2.b95cfb3e.chunk.css"
   },
   {
-    "revision": "234052029c2162cd5c4b",
+    "revision": "115845f2e6c27433fe37",
     "url": "/NrityaGurukul/static/css/main.cec8ac06.chunk.css"
   },
   {
-    "revision": "411c49aa8ebd262458c4",
-    "url": "/NrityaGurukul/static/js/2.eaea464b.chunk.js"
+    "revision": "92aaf8dffb21cebb76fc",
+    "url": "/NrityaGurukul/static/js/2.db6dd406.chunk.js"
   },
   {
     "revision": "213bca7ce55773509172dbee394af2a1",
-    "url": "/NrityaGurukul/static/js/2.eaea464b.chunk.js.LICENSE.txt"
+    "url": "/NrityaGurukul/static/js/2.db6dd406.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "234052029c2162cd5c4b",
-    "url": "/NrityaGurukul/static/js/main.64e2fdf5.chunk.js"
+    "revision": "115845f2e6c27433fe37",
+    "url": "/NrityaGurukul/static/js/main.b3775420.chunk.js"
   },
   {
     "revision": "1faafa02978077e393df",
--- src/Components/Text/Body.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'body.css';
-
-const Body = (props) => {
-  return <h3 className = 'bodyText'>{props.childen}</h3>
-}
-
-export default Body;
