51bacdc340c555bfd7595b1873204e7fd062b76a
unknown <aashrav.shetty01@gmail.com>
2023-08-02 00:58:21 -0700

    Updates

---
+++ precache-manifest.a2b77cf90f3f2d4b5ab2d6d8eb008cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb80a4cfeb429e2c70e9c8306f207a56",
+    "url": "/NrityaGurukul/index.html"
+  },
+  {
+    "revision": "411c49aa8ebd262458c4",
+    "url": "/NrityaGurukul/static/css/2.b95cfb3e.chunk.css"
+  },
+  {
+    "revision": "234052029c2162cd5c4b",
+    "url": "/NrityaGurukul/static/css/main.cec8ac06.chunk.css"
+  },
+  {
+    "revision": "411c49aa8ebd262458c4",
+    "url": "/NrityaGurukul/static/js/2.eaea464b.chunk.js"
+  },
+  {
+    "revision": "213bca7ce55773509172dbee394af2a1",
+    "url": "/NrityaGurukul/static/js/2.eaea464b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "234052029c2162cd5c4b",
+    "url": "/NrityaGurukul/static/js/main.64e2fdf5.chunk.js"
+  },
+  {
+    "revision": "1faafa02978077e393df",
+    "url": "/NrityaGurukul/static/js/runtime-main.7c5060b3.js"
+  }
+]);
